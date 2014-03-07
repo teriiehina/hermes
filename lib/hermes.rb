95a2689a6b6bdc4d244c607ff3ad74c9b8c869c1
Peter Meuel <pmeuel.externe@pagesjaunes.fr>
2014-03-07 10:46:25 +0100

    Update the CLI to use a plist as the sole argument

--- lib/hermes.rb
+++ lib/hermes.rb
@@ -2,6 +2,8 @@ require "hermes/version"
 require 'thor'
 require 'plist'
 
+require 'hermes/deploy'
+
 module Hermes
 
   class Hermes < Thor
@@ -9,15 +11,15 @@ module Hermes
     desc "check", "will check if everything necessary for building easily is present"
     def check
       puts "checking. Missing tools will be installed, if possible."
-
+      
     end
 
     desc "build JOB", "will build the job"
     def build(plist)
       
-      puts "building using the file #{job}"
+      puts "building using the file #{plist}"
       
-      deployments     = Plist::parse_xml(plist)
+      deployments = Plist::parse_xml(plist)
 
       deploy deployments
     end
