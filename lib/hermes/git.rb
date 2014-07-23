e1aa2b567c3aecef310ed7b04a2ae2bc32adeacf
Peter Meuel <pmeuel.externe@pagesjaunes.fr>
2014-07-23 14:47:04 +0200

    Add a method to tag and checkout the correct version of the code source

--- lib/hermes/git.rb
+++ lib/hermes/git.rb
@@ -1,15 +1,38 @@
 require 'rubygems'
 require 'bundler/setup'
 
-def tagGit (settings)
-
-  return
+def checkOutGitVersion (settings)
   
-  tag_name    = "jenkins_" + settings[:buildNumber]
-  create_tag  = "git tag #{tag_name}"
-  push_tag    = "git push --tags"
+  tags            = `git tag`
+  tag_name        = settings[:CFBundleVersion]
+  current_commit  = settings[:gitSHA1]
+  
+  if tags.include?("#{tag_name}\n") == false
+    puts "Le tag #{tag_name} n'existe pas."
+    puts "Nous allons le créer pour pointer vers le commit actuel (#{current_commit})"
 
-  system(create_tag)
-  system(push_tag)
+    create_tag = `git tag #{tag_name}`
+  end
+  
+  puts "On checkout le tag #{tag_name}"
+  co_tag = `git checkout #{tag_name}`
+end
+
+def tagGit (settings)
+  
+  tag_name       = settings[:CFBundleVersion]
+  current_commit = settings[:gitSHA1]
+  
+  # si le tag existe déjà on l'efface
+  tags = `git tag`
+  
+  if tags.include?("#{tag_name}\n")
+    puts "Le tag #{tag_name} existe déjà."
+    puts "Nous allons le faire pointer vers le commit actuel (#{current_commit})"
+
+    delete_tag = `git tag -d #{tag_name}`
+  end
+  
+  create_tag = `git tag #{tag_name}`
 
 end
