0936c8e5819c22be5d9bc8666abb47fd23f7a22d
Peter Meuel <pmeuel.externe@pagesjaunes.fr>
2014-07-23 14:47:51 +0200

    Add more settings

--- lib/hermes/settings.rb
+++ lib/hermes/settings.rb
@@ -9,26 +9,25 @@ def load_settings(deploy)
   settings[:deploy]               = deploy
   
   settings[:applicationName]      = deploy["infosPlist"]["CFBundleDisplayName"]
-  settings[:projectDirectory]     = deploy["paths"]["projectAbsolutPath"]
-  settings[:projectDirectory]     = Dir.pwd
-  
+  settings[:CFBundleVersion]      = deploy["infosPlist"]["CFBundleVersion"]
 
+  settings[:projectDirectory]     = Dir.pwd
+  settings[:projectInfosPath]     = settings[:projectDirectory] + "/" + deploy["paths"]["infosPlistRelativePath"]
+  
+  settings[:bundleName]           = deploy["build"]["projectName"]
   settings[:workspaceName]        = deploy["build"]["workspaceName"]
   settings[:schemeName]           = deploy["build"]["schemeName"]
-  settings[:projectInfosPath]     = settings[:projectDirectory] + "/" + deploy["paths"]["infosPlistRelativePath"]
-
   settings[:targetSDK]            = deploy["build"]["targetSDK"]
+
   settings[:buildConfiguration]   = deploy["build"]["buildConfiguration"]
   settings[:buildDirectory]       = settings[:projectDirectory] + "/" + deploy["paths"]["buildRelativePath"]
-  settings[:buildNumber]          = deploy["build"]["buildNumber"]
   settings[:buildNumber]          = `git rev-list --max-count=1 HEAD`[0..7]
+
+  settings[:gitSHA1]              = `git rev-list --max-count=1 HEAD`[0..7]
 
   settings[:signingIdentity]      = deploy["signing"]["identity"]
   settings[:provisioningProfile]  = "\"#{settings[:projectDirectory]}/#{deploy["signing"]["profile"]}\""
-  
-  
-  settings[:bundleName]           = deploy["build"]["projectName"]
-  
+    
   settings
 
 end
